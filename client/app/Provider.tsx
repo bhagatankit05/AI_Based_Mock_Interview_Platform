ee39f1e0b1d7bfb3acd93ce6629c60f57430fe54
bhagatankit05 <bhagatankit82@gmail.com>
2025-08-16 16:18:59 +0530

    implemented insert new user into Convex DB only if not already present

--- client/app/Provider.tsx
+++ client/app/Provider.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { api } from '@/convex/_generated/api'
+import { useUser } from '@clerk/nextjs'
+import { useMutation } from 'convex/react'
+import React, { useEffect } from 'react'
 
-const Provider = ({children}:any) => {
-  return (
-    <div>
-      <div>{children}</div>
-    </div>
-  )
+const Provider = ({ children }: any) => {
+
+    const { user } = useUser();
+    const CreateUser = useMutation(api.users.CreateNewUser);
+
+    useEffect(() => {
+        user && CreateNewUser();
+
+    }, [user])
+
+
+
+    const CreateNewUser = async () => {
+
+        if (user) {
+            const result = await CreateUser({
+                email: user?.primaryEmailAddress?.emailAddress ?? '',
+                imageUrl: user?.imageUrl,
+                name: user?.fullName ?? ''
+
+            })
+            console.log(result)
+        }
+    }
+
+    return (
+        <div>
+            <div>{children}</div>
+        </div>
+    )
 }
 
 export default Provider
