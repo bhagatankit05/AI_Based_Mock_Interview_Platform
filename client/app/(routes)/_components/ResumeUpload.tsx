8f25e0e96453291f11db5f5d7fa0c14b7583b7dc
bhagatankit05 <bhagatankit82@gmail.com>
2025-08-17 13:55:30 +0530

    added option in dialog for uploading resume

---
+++ client/app/(routes)/_components/ResumeUpload.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { FileUpload } from '@/components/ui/file-upload'
+import React, { useState } from 'react'
+
+const ResumeUpload = () => {
+    const [files, setFiles] = useState<File[]>([]);
+  const handleFileUpload = (files: File[]) => {
+    setFiles(files);
+    console.log(files);
+  };
+    return (
+        <div>
+            <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg">
+                <FileUpload onChange={handleFileUpload} />
+            </div>
+
+        </div>
+    )
+}
+
+export default ResumeUpload
--- client/app/(routes)/dashboard/EmptyState.tsx
+++ client/app/(routes)/dashboard/EmptyState.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import React from 'react'
+import CreateInterviewDialog from '../_components/CreateInterviewDialog'
 
 const EmptyState = () => {
   return (
@@ -12,8 +13,7 @@ const EmptyState = () => {
         height={330} 
       />
       <h2 className='mt-2 text-lg text-gray-500'>You do not have any Interview created.</h2>
-
-      <Button className=''>+ Create Interview</Button>
+      <CreateInterviewDialog/>
     </div>
   )
 }
