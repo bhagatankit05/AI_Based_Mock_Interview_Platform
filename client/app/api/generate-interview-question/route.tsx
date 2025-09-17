250d8eec9ceddb7352ea58aa602807c05bdb2361
bhagatankit05 <bhagatankit82@gmail.com>
2025-09-17 22:04:21 +0530

    feat: integrate Arcjet bot protection and user validation in interview question generation

--- client/app/api/generate-interview-question/route.tsx
+++ client/app/api/generate-interview-question/route.tsx
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import ImageKit from "imagekit";
 import axios from "axios";
+import { aj } from "@/utils/arcjet";
+import { currentUser } from "@clerk/nextjs/server";
 
 const imagekit = new ImageKit({
     publicKey: process.env.IMAGEKIT_URL_PUBLIC_KEY as string,
@@ -10,10 +12,24 @@ const imagekit = new ImageKit({
 
 export async function POST(req: NextRequest) {
     try {
+        const user = await currentUser();
         const formData = await req.formData();
         const file = formData.get("file") as File | null;
         const jobTitle = formData.get("jobTitle") as string | null;
         const jobDescription = formData.get("jobDescription") as string | null;
+
+        const decision = await aj.protect(req, {userId:user?.primaryEmailAddress?.emailAddress??'', requested: 5 }); // Deduct 5 tokens from the bucket
+          console.log("Arcjet decision", decision);
+        
+          //@ts-ignore
+          if (decision?.reason?.remaining==0 ) {
+            return NextResponse.json(
+              { 
+                status:429,
+                result:'No free credit remaining, try again after 24hrs',
+               },
+            );
+          }
 
         if (file) {
             const arrayBuffer = await file.arrayBuffer();
