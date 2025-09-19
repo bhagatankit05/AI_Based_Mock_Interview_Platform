7e0d290758047d53e9871de707b6ad71ae2c2810
bhagatankit05 <bhagatankit82@gmail.com>
2025-09-19 22:10:46 +0530

    feat: update Interview page layout with email input and send button

--- client/app/(routes)/interview/[interviewId]/page.tsx
+++ client/app/(routes)/interview/[interviewId]/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
-import { ArrowRight } from 'lucide-react'
+import { Input } from '@/components/ui/input'
+import { ArrowRight, Send } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
@@ -7,7 +8,7 @@ const Interview = () => {
   return (
     <div className='flex flex-col items-center justify-center mt-24'>
       <div className='max-w-3xl w-full'>
-        <Image src={'/'} alt="Interview Image" width={600} height={400} className='w-full h-[200px] object-cover' />
+        <Image src={'/interview_st.jpg'} alt="Interview Image" width={600} height={400} className='w-full h-[300px] object-cover' />
         <div className='p-6 flex flex-col items-center space-y-5'>
           <h2 className='font-bold text-3xl text-center'>
             Ready to Start Interview?
@@ -16,7 +17,14 @@ const Interview = () => {
           <Button>Start Interview <ArrowRight /></Button>
 
           <hr />
-          
+          <div className='p-3 bg-gray-50 rounded-2xl'>
+          <h2 className='font-semibold text-2xl'>Want to sent interview link to someone?</h2>
+          <div className='flex gap-5 w-full items-center max-w-xl mt-2'>
+          <Input placeholder='Enter email address' className='w-full max-w-xl' />
+          <Button> <Send/> </Button>
+          </div>
+          </div>
+        
         </div>
       </div>
     </div>
