1086527dffb66768abf64bb3bc5ea32e4df3af20
bhagatankit05 <bhagatankit82@gmail.com>
2025-09-20 19:49:36 +0530

    feat: enhance StartInterview component with interview data fetching logic

--- client/app/(routes)/interview/[interviewId]/start/page.tsx
+++ client/app/(routes)/interview/[interviewId]/start/page.tsx
@@ -2,22 +2,38 @@
 import { api } from '@/convex/_generated/api';
 import { useConvex } from 'convex/react';
 import { useParams } from 'next/navigation';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
+type InterviewData = {
+  jobTitle: string | null,
+  jobDescription: string | null,
+  interviewQuestions: InterviewQuestions[],
+  userId: string | null,
+  _id: string
+}
+
+type InterviewQuestions = {
+  answer: string,
+  question: string
+}
 const StartInterview = () => {
-    const {interviewId} = useParams();
-    const convex = useConvex();
+  const { interviewId } = useParams();
+  const convex = useConvex();
+  const [interviewData, setInterviewData] = useState<InterviewData>();
 
-    useEffect(()=>{
-        GetInterviewQuestions();
-    },[interviewId])
 
-    const GetInterviewQuestions=async ()=>{
-        const result = await convex.query(api.Interview.GetInterviewQuestions,{
-            //@ts-ignore
-            interviewRecordId:interviewId 
-        })
-    }
+  useEffect(() => {
+    GetInterviewQuestions();
+  }, [interviewId])
+
+  const GetInterviewQuestions = async () => {
+    const result = await convex.query(api.Interview.GetInterviewQuestions, {
+      //@ts-ignore
+      interviewRecordId: interviewId
+    });
+    console.log(result);
+    setInterviewData(result);
+  }
   return (
     <div>
       start interview
